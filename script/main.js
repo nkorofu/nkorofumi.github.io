f1da05ed310767b6273ffe591df30d5b68dfd7d2
nkorofu <mitsubatti24@gmail.com>
2024-09-30 01:14:53 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -193,7 +193,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .from("****", 0.5, {
+    .from(".hat", 0.5, {
       x: -100,
       y: 350,
       rotation: -180,
@@ -303,4 +303,3 @@ const resolveFetch = () => {
 };
 
 resolveFetch().then(animationTimeline());
-
